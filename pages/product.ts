493c47ce7eb3cc95024276c94f40d8a94334e0b8
Piotr Gutowski <piotr.gutowski099@gmail.com>
2024-06-30 21:59:09 +0200

    added remaining testcases for product page

--- pages/product.ts
+++ pages/product.ts
@@ -10,48 +10,128 @@ export class ProductPage {
   readonly mensTshirtButton: Locator;
   readonly ladiesTshirtButton: Locator;
   readonly backButton: Locator;
+  readonly productImg: Locator;
+  readonly productName: Locator;
+  readonly productPrice: Locator;
+  readonly sizeSelect: Locator;
+  readonly quantitySelect: Locator;
+  readonly productDescription: Locator;
+  readonly addToCartButton: Locator;
+  readonly dialogPopup: Locator;
+  readonly populatedCartButton: Locator;
+  readonly viewCartButton: Locator;
+  readonly checkoutButton: Locator;
+  readonly closeDialogButton: Locator;
 
     constructor(page: Page){
-        this.page = page;
-        this.shopButton = page.getByLabel('SHOP Home');
-        this.cartButton = page.getByLabel('Shopping cart: 0 items');
-        this.mensOuterwearButton = page.locator('#tabContainer').getByRole('link', { name: 'Men\'s Outerwear' });
-        this.ladiesOuterwearButton = page.locator('#tabContainer').getByRole('link', { name: 'Ladies Outerwear' });
-        this.mensTshirtButton = page.locator('#tabContainer').getByRole('link', { name: 'Men\'s T-Shirts' });
-        this.ladiesTshirtButton = page.locator('#tabContainer').getByRole('link', { name: 'Ladies T-Shirts' });
-        this.backButton = page.getByLabel('Go back');
+      this.page = page;
+      this.shopButton = page.getByLabel('SHOP Home');
+      this.cartButton = page.getByLabel('Shopping cart: 0 items');
+      this.mensOuterwearButton = page.locator('#tabContainer').getByRole('link', { name: 'Men\'s Outerwear' });
+      this.ladiesOuterwearButton = page.locator('#tabContainer').getByRole('link', { name: 'Ladies Outerwear' });
+      this.mensTshirtButton = page.locator('#tabContainer').getByRole('link', { name: 'Men\'s T-Shirts' });
+      this.ladiesTshirtButton = page.locator('#tabContainer').getByRole('link', { name: 'Ladies T-Shirts' });
+      this.backButton = page.getByLabel('Go back');
+      this.productImg = page.getByRole('img', { name: 'Men\'s Tech Shell Full-Zip' });
+      this.productName = page.getByRole('heading', { name: 'Men\'s Tech Shell Full-Zip' });
+      this.productPrice = page.locator('.price');
+      this.sizeSelect = page.locator('#sizeSelect');
+      this.quantitySelect = page.locator('#quantitySelect');
+      this.productDescription = page.locator('.description');
+      this.addToCartButton = page.getByLabel('Add this item to cart');
+      this.dialogPopup = page.getByRole('dialog');
+      this.populatedCartButton = page.getByLabel('Shopping cart: 1 item');
+      this.viewCartButton = page.getByRole('link', {name: 'View Cart'});
+      this.checkoutButton = page.getByRole('link', {name: 'Checkout'});
+      this.closeDialogButton = page.getByLabel('Close dialog');
     }
 
-  async goToProductPage(){
-    await this.page.goto('https://shop.polymer-project.org/detail/mens_outerwear/Men+s+Tech+Shell+Full-Zip');
-  }
+    async goToProductPage(){
+      await this.page.goto('https://shop.polymer-project.org/detail/mens_outerwear/Men+s+Tech+Shell+Full-Zip');
+    }
 
-  async clickShopButton() {
-    await this.shopButton.click();
-  }
+    async clickShopButton() {
+      await this.shopButton.click();
+    }
 
-  async clickCartButton() {
-    await this.cartButton.click();
-  }
+    async clickCartButton() {
+      await this.cartButton.click();
+    }
 
-  async clickMensOuterwearButton() {
-    await this.mensOuterwearButton.click();
-  }
+    async clickMensOuterwearButton() {
+      await this.mensOuterwearButton.click();
+    }
 
-  async clickLadiesOuterwearButton() {
-    await this.ladiesOuterwearButton.click();
-  }
+    async clickLadiesOuterwearButton() {
+      await this.ladiesOuterwearButton.click();
+    }
 
-  async clickMensTshirtsButton() {
-    await this.mensTshirtButton.click();
-  }
+    async clickMensTshirtsButton() {
+      await this.mensTshirtButton.click();
+    }
 
-  async clickLadiesTshirtsButton() {
-    await this.ladiesTshirtButton.click();
-  }
+    async clickLadiesTshirtsButton() {
+      await this.ladiesTshirtButton.click();
+    }
 
-  async clickBackButton() {
-    await this.backButton.click();
-  }
+    async clickBackButton() {
+      await this.backButton.click();
+    }
+
+    async selectSizeOptionXS() {
+      await this.sizeSelect.selectOption('XS');
+    }
+    
+    async selectSizeOptionS() {
+      await this.sizeSelect.selectOption('S');
+    }
+
+    async selectSizeOptionM() {
+      await this.sizeSelect.selectOption('M');
+    }
+
+    async selectSizeOptionL() {
+      await this.sizeSelect.selectOption('L');
+    }
+
+    async selectSizeOptionXL() {
+      await this.sizeSelect.selectOption('XL');
+    }
+
+    async selectQuantityOption1() {
+      await this.quantitySelect.selectOption('1');
+    }
+
+    async selectQuantityOption2() {
+      await this.quantitySelect.selectOption('2');
+    }
+
+    async selectQuantityOption3() {
+      await this.quantitySelect.selectOption('3');
+    }
+
+    async selectQuantityOption4() {
+      await this.quantitySelect.selectOption('4');
+    }
+
+    async selectQuantityOption5() {
+      await this.quantitySelect.selectOption('5');
+    }
+
+    async clickAddToCartButton() {
+      await this.addToCartButton.click();
+    }
+
+    async clickViewCartButton() {
+      await this.viewCartButton.click();
+    }
+
+    async clickCheckoutButton() {
+      await this.checkoutButton.click();
+    }
+
+    async clickCloseDialogButton() {
+      await this.closeDialogButton.click();
+    }
 
 }
--- smoke/smoke-test-product.spec.ts
+++ smoke/smoke-test-product.spec.ts
@@ -141,7 +141,7 @@ test.describe('Smoke Test - Product page', () => {
       await productPage.clickMensTshirtsButton();
 
       await expect(productPage.page).toHaveURL('https://shop.polymer-project.org/list/mens_tshirts');
-
+      
     });
 
   });
@@ -189,6 +189,296 @@ test.describe('Smoke Test - Product page', () => {
       await productPage.clickBackButton();
 
       await expect(productPage.page).toHaveURL('https://shop.polymer-project.org/list/mens_outerwear');
+
+    });
+
+  });
+
+  test('Scenario: Product image is visible', async ({ productPage }) => {
+
+    await test.step('AC: The image is visible', async () => {
+
+      await expect(productPage.productImg).toBeVisible();
+
+    });
+
+    await test.step('AC: The image is editable', async () => {
+
+      await expect(productPage.productImg).toBeEditable();
+
+    });
+
+  });
+
+  test('Scenario: Product has correct name', async ({ productPage }) => {
+
+    await test.step('AC: The name is visible', async () => {
+
+      await expect(productPage.productName).toBeVisible();
+
+    });
+
+    await test.step('AC: The name is editable', async () => {
+
+      await expect(productPage.productName).toBeEditable();
+
+    });
+
+    await test.step('AC: Correct name is displayed - Men\'s Tech Shell Full-Zip', async () =>{
+
+      await expect(productPage.productName).toHaveText(/Men's Tech Shell Full-Zip/);
+
+    });
+
+  });
+
+  test('Scenario: Product has correct price', async ({ productPage }) => {
+
+    await test.step('AC: The price is visible', async () => {
+
+      await expect(productPage.productPrice).toBeVisible();
+
+    });
+
+    await test.step('AC: The price is editable', async () => {
+
+      await expect(productPage.productPrice).toBeEditable();
+
+    });
+
+    await test.step('AC: Correct price is displayed - $50.20', async () => {
+
+      await expect(productPage.productPrice).toHaveText(/\$50.20/);
+
+    });
+
+  });
+
+  test('Scenario: Size option is interactable', async ({ productPage }) => {
+        
+    await test.step('AC: Size option is visible', async () => {
+
+      await expect(productPage.sizeSelect).toBeVisible();
+
+    });
+
+    await test.step('AC: Size option is editable', async () => {
+
+      await expect(productPage.sizeSelect).toBeEditable();
+
+    });
+
+    await test.step('AC: Correct sizes are available - XS', async () => {
+
+      await productPage.selectSizeOptionXS();
+
+      await expect(productPage.sizeSelect).toHaveValue(/XS/);
+
+    });
+
+    await test.step('AC: Correct sizes are available - S', async () => {
+
+      await productPage.selectSizeOptionS();
+
+      await expect(productPage.sizeSelect).toHaveValue(/S/);
+
+    });
+
+    await test.step('AC: Correct sizes are available - M', async () => {
+
+      await productPage.selectSizeOptionM();
+
+      await expect(productPage.sizeSelect).toHaveValue(/M/);
+
+    });
+
+    await test.step('AC: Correct sizes are available - L', async () => {
+
+      await productPage.selectSizeOptionL();
+
+      await expect(productPage.sizeSelect).toHaveValue(/L/);
+
+    });
+
+    await test.step('AC: Correct sizes are available - XL', async () => {
+
+      await productPage.selectSizeOptionXL();
+
+      await expect(productPage.sizeSelect).toHaveValue(/XL/);
+
+    });
+
+  });
+
+  test('Scenario: Quantity option is interactable', async ({ productPage }) => {
+        
+    await test.step('AC: Size option is visible', async () => {
+
+      await expect(productPage.quantitySelect).toBeVisible();
+
+    });
+
+    await test.step('AC: Size option is editable', async () => {
+
+      await expect(productPage.quantitySelect).toBeEditable();
+
+    });
+
+    await test.step('AC: Correct quantities are available - 1', async () => {
+
+      await productPage.selectQuantityOption1();
+
+      await expect(productPage.quantitySelect).toHaveValue(/1/);
+
+    });
+
+    await test.step('AC: Correct quantities are available - 2', async () => {
+
+      await productPage.selectQuantityOption2();
+
+      await expect(productPage.quantitySelect).toHaveValue(/2/);
+
+    });
+
+    await test.step('AC: Correct quantities are available - 3', async () => {
+
+      await productPage.selectQuantityOption3();
+
+      await expect(productPage.quantitySelect).toHaveValue(/3/);
+
+    });
+
+    await test.step('AC: Correct quantities are available - 4', async () => {
+
+      await productPage.selectQuantityOption4();
+
+      await expect(productPage.quantitySelect).toHaveValue(/4/);
+
+    });
+
+    await test.step('AC: Correct quantities are available - 5', async () => {
+
+      await productPage.selectQuantityOption5();
+
+      await expect(productPage.quantitySelect).toHaveValue(/5/);
+
+    });
+
+  });
+
+  test('Scenario: Product description is visible', async({ productPage }) => {
+
+    await test.step('AC: Description box is displayed', async () => {
+
+      await expect(productPage.productDescription).toBeVisible();
+
+    });
+
+  });
+
+  test('Scenario: Add to cart button is interactable', async ({ productPage }) => {
+
+    await test.step('AC: The button is visible', async () => {
+
+      await expect(productPage.addToCartButton).toBeVisible();
+
+    });
+
+    await test.step('AC: The button is editable', async() => {
+
+      await expect(productPage.addToCartButton).toBeEditable();
+
+    });
+
+    await test.step('AC: The button can be clicked', async () => {
+      
+      await productPage.clickAddToCartButton();
+
+      await expect(productPage.dialogPopup).toBeVisible();
+
+    });
+
+    await test.step('AC: The item was added to the cart', async() => {
+
+      await expect(productPage.populatedCartButton).toBeVisible();
+
+    });
+
+  });
+
+  test('Scenario: View cart button is interactable on the popup', async ({ productPage }) => {
+
+    await test.step('AC: The button is visible', async () => {
+
+      await productPage.clickAddToCartButton();
+
+      await expect(productPage.viewCartButton).toBeVisible();
+
+    });
+
+    await test.step('AC: The button is editable', async () => {
+
+      await expect(productPage.viewCartButton).toBeEditable();
+
+    });
+
+    await test.step('AC: The user is redirected to the cart page', async () => {
+
+      await productPage.clickViewCartButton();
+
+      await expect(productPage.page).toHaveURL('https://shop.polymer-project.org/cart');
+
+    });
+
+  });
+
+  test('Scenario: Checkout button is interactable on the popup', async ({ productPage }) => {
+
+    await test.step('AC: The button is visible', async () => {
+
+      await productPage.clickAddToCartButton();
+      
+      await expect(productPage.checkoutButton).toBeVisible();
+
+    });
+
+    await test.step('AC: The button is editable', async () => {
+
+      await expect(productPage.checkoutButton).toBeEditable();
+
+    });
+
+    await test.step('AC: The user is redirected to the checkout page', async () => {
+
+      await productPage.clickCheckoutButton();
+
+      await expect(productPage.page).toHaveURL('https://shop.polymer-project.org/checkout');
+
+    });
+
+  });
+
+  test('Scenario: Close button is interactable on the popup', async ({ productPage }) => {
+
+    await test.step('AC: The button is visible', async () => {
+
+      await productPage.clickAddToCartButton();
+      
+      await expect(productPage.closeDialogButton).toBeVisible();
+
+    });
+
+    await test.step('AC: The button is editable', async () => {
+
+      await expect(productPage.closeDialogButton).toBeEditable();
+
+    });
+
+    await test.step('AC: The popup closes after clicking the button', async () => {
+
+      await productPage.clickCloseDialogButton();
+
+      await expect(productPage.dialogPopup).not.toBeVisible();
 
     });
 
